927e77ee0e841f1d5914fcd398ca48a1cfa58c27
Hansanghyeon <dandan9509@gmail.com>
2021-06-09 14:01:10 +0900

    :sparkles: synology default 80, 443 port 사용 nginx 포트 변경

---
+++ change-ports.sh
@@ -0,0 +1,9 @@
+HTTP_PORT=80
+HTTP_PATCH_PORT=81
+HTTPS_PORT=443
+HTTPS_PATCH_PORT=444
+
+sed -i "s/^\( *listen .*\)$HTTP_PATCH_PORT/\1$HTTP_PORT/" /usr/syno/share/nginx/*.mustache
+sed -i "s/^\( *listen .*\)$HTTP_PORT/\1$HTTP_PATCH_PORT/" /usr/syno/share/nginx/*.mustache
+sed -i "s/^\( *listen .*\)$HTTPS_PATCH_PORT/\1$HTTPS_PORT/" /usr/syno/share/nginx/*.mustache
+sed -i "s/^\( *listen .*\)$HTTPS_PORT/\1$HTTPS_PATCH_PORT/" /usr/syno/share/nginx/*.mustache
